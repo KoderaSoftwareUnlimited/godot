98cef10cc5a5707dfd706f8132ecac169db133d6
koder <mariusz@chwalba.net>
2018-07-03 16:56:17 +0200

    Working one

--- compile-and-share.bat
+++ compile-and-share.bat
@@ -1,3 +1,5 @@
 scons -j4 platform=windows target=release_debug tools=yes
 scons -j4 platform=windows target=release tools=no
-cp bin/* s:/Dropbox/chmura.kodera.pl/projekty/delta-v/software/
+
+
+copy bin\* s:\Dropbox\chmura.kodera.pl\projekty\delta-v\software\
