b2945160833393eae109102b4e4b11c38b42e5d1
koder <mariusz@chwalba.net>
2018-07-04 16:18:17 +0200

    Fixes builder

--- compile-and-share.bat
+++ compile-and-share.bat
@@ -1,5 +1,7 @@
-scons -j4 platform=windows target=release_debug tools=yes
-scons -j4 platform=windows target=release tools=no
 
+
+call scons -j4 platform=windows target=release_debug tools=yes
+
+call scons -j4 platform=windows target=release tools=no
 
 copy bin\* s:\Dropbox\chmura.kodera.pl\projekty\delta-v\software\
